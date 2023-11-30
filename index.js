b74e8bc2a8e30bb3f891c083247e969e5542b826
Shahjalal2075 <shahjalal2075@gmail.com>
2023-12-01 04:05:23 +0600

    admin hr employee verify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,13 +11,13 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 console.log('UserName: ', process.env.DB_USER);
 console.log('Password: ', process.env.DB_PASS);
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cookieParser = require('cookie-parser');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s6b6iw5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +29,8 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -36,32 +39,76 @@ async function run() {
     const database = client.db("employeeDB");
     const usersCollectionUsers = database.collection("users");
     const usersCollectionTestimonials = database.collection("testimonials");
+    const usersCollectionWorkSheets = database.collection("workSheets");
 
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       res
-      .cookie('token',token,{
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none'
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+        })
+        .send({ token });
+    })
+
+    const verifyToken = async (req, res, next) => {
+      const token = req.cookies?.token;
+      if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
       })
-      .send({success: true});
-    })
+    }
 
-    app.post('/logout', async(req,res) => {
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollectionUsers.findOne(query);
+      const isAdmin = user?.role === 'Admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    const verifyHr = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollectionUsers.findOne(query);
+      const isAdmin = user?.role === 'HR';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    app.post('/logout', async (req, res) => {
       const user = req.body;
-      res.clearCookie('token',{maxAge: 0}).send({success: true});
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true });
     })
 
-    app.get('/users', async (req, res) => {
+    app.get('/users/:role/:id',verifyToken,verifyHr, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollectionUsers.findOne(query);
+      res.send(result);
+    })
+
+    app.get('/users',verifyToken,verifyAdmin, async (req, res) => {
       const cursor = usersCollectionUsers.find()
       const result = await cursor.toArray();
       res.send(result);
     })
 
-    app.get('/users/:role', async (req, res) => {
+    app.get('/users/:role',verifyToken,verifyHr, async (req, res) => {
       const role = req.params.role;
       const query = { role: role }
       const cursor = usersCollectionUsers.find(query);
@@ -69,11 +116,12 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/users/:role1/:role2', async (req, res) => {
+    app.get('/users/:role1/:role2/:role3',verifyToken,verifyAdmin, async (req, res) => {
       const role1 = req.params.role1;
       const role2 = req.params.role2;
-      const query1 = { role: role1}
-      const query2 = { role: role2}
+      const query1 = { role: role1 }
+      const query2 = { role: role2 }
+      console.log('token', req.cookies.token)
       const cursor1 = usersCollectionUsers.find(query1);
       const cursor2 = usersCollectionUsers.find(query2)
       const result1 = await cursor1.toArray();
@@ -82,19 +130,28 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+    app.patch('/users/:email',verifyToken, async (req, res) => {
+      const id = req.params.email;
+      const query = { email: id }
       const updateEmployee = req.body;
-      console.log(updateEmployee);
+      console.log(query);
       const updateDoc = {
         $set: {
-          verify: updateEmployee.verify
+          verify: updateEmployee.verify,
+          role: updateEmployee.role
         }
       }
       const result = await usersCollectionUsers.updateOne(query, updateDoc);
       res.send(result);
     })
+
+    app.delete('/users/:id',verifyToken,verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: new ObjectId(id) };
+      const result = await usersCollectionUsers.deleteOne(cursor);
+      res.send(result);
+    })
+
 
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -103,11 +160,19 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/worksheets', async (req, res) => {
+      const cursor = usersCollectionWorkSheets.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.get('/testimonials', async (req, res) => {
       const cursor = usersCollectionTestimonials.find()
       const result = await cursor.toArray();
       res.send(result);
     })
+
+
 
     /*
  
